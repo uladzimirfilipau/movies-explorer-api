d94f588a31d80e6d36bf3bd7f530f1cbfe8797e0
Vova <uladzimirfilipau@gmail.com>
2022-11-06 11:52:55 +0300

    fix: config.js
    - add process.env.PORT
    - change MONGODB link

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,8 @@ const {
   NODE_ENV = 'dev',
   JWT_SECRET = 'dev-secret',
   SALT = 10,
-  PORT = 3001,
-  MONGODB = 'mongodb://localhost:27017/filmsdb',
+  PORT = process.env.PORT || 3001,
+  MONGODB = 'mongodb+srv://ulfi:lQEF8JvuqAX0yAWV@cluster0.ozqtuja.mongodb.net/?retryWrites=true&w=majority',
 } = process.env;
 
 module.exports = {
